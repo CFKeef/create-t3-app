a4702f8efb13291efa7111706fb3e5a93a23f662
CFKeef <Patryckg@outlook.com>
2022-12-27 19:21:46 -0500

    chore: got rid of the role on the moon image

--- www/src/components/navigation/ThemeDropdown.tsx
+++ www/src/components/navigation/ThemeDropdown.tsx
@@ -70,7 +70,6 @@ export default function ThemeDropdown() {
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-6 w-6 fill-current stroke-current transition-colors duration-300"
                   viewBox="0 0 20 20"
-                  role="img"
                 >
                   <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
                 </svg>
